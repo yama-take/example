2a5bf5e6caa30308d3f9268de6b10811c8341f07
takeshi yamazaki <y.take4@gmail.com>
2015-12-26 14:54:50 +0900

    scala 初回コミット

---
+++ src/HelloWorld.scala
@@ -0,0 +1,24 @@
+
+
+object HelloWorld {
+  
+  val ary = List(1,2,3,4,5,6,7,8,9,10)
+  
+  val strList = List("東京都","神奈川県","千葉")
+  
+  def main(args: Array[String]): Unit = {
+//     for(e <- ary){
+//       println(e)
+//     }
+//     println("---------")
+//     
+//     ary.foreach {e => println(e)}
+     
+     println("---------")
+     ary.zipWithIndex.foreach { case(e: Int, i: Int) => println("%d: %s".format(i, e)) }
+     
+     println("---------")
+     println(strList.toString())
+     strList.zipWithIndex.foreach{ case(e: String, i: Int) => println("%d: %s".format(i, e))}
+  }
+}
